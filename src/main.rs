b8987fa38d374ccf0f26546896a4fd508ef6dec7
sauce-Git <dadad1004@naver.com>
2024-09-26 22:13:36 +0900

    fix: delete rabbitmq

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
-use anyhow::Result;
 use axum::{
     routing::{get, post},
     Router,
 };
 use dotenv::dotenv;
-use futures::{stream::Next, StreamExt};
-use lapin::{
-    message::DeliveryResult,
-    options::{BasicAckOptions, BasicConsumeOptions, QueueDeclareOptions},
-    types::FieldTable,
-    Connection, ConnectionProperties,
-};
 use routes::issue::issue_session;
-use serde::Deserialize;
-use services::{file_manager_service::ModifyFile, FileManagerService};
 use std::{
     env,
     net::{IpAddr, SocketAddr},
 };
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
-};
+use tokio::net::TcpListener;
 
 mod layers;
 mod routes;
@@ -60,99 +47,7 @@ async fn main() {
     ));
     let server = TcpListener::bind(&addr).await.unwrap();
 
-    // serve microservice
-    //println!("Microservice is running on http://{}", addr);
-    //tokio::spawn(microservice());
-
-    // serve rabbit
-    tokio::spawn(rabbit());
-
     // serve axum
     println!("Axum is running on http://{}", addr);
     axum::serve(server, router).await.unwrap();
-}
-
-#[derive(Deserialize)]
-#[allow(dead_code)]
-struct MergeRequestData {
-    file_key: String,
-    total_chunk: u32,
-}
-
-#[derive(Deserialize)]
-#[allow(dead_code)]
-struct DeleteRequestData {
-    file_key: String,
-}
-
-#[derive(Deserialize)]
-enum MicroRequestData {
-    Merge(MergeRequestData),
-    Delete(DeleteRequestData),
-}
-
-#[derive(Deserialize)]
-struct RequestPattern {
-    target: String,
-    cmd: String,
-}
-
-#[derive(Deserialize)]
-#[allow(dead_code)]
-struct MicroRequest {
-    pattern: RequestPattern,
-    data: MicroRequestData,
-}
-
-enum RabbitResponseStatus {
-    Success,
-    Fail,
-}
-
-struct RabbitResponse {
-    status: RabbitResponseStatus,
-    message: String,
-}
-
-async fn rabbit() -> Result<()> {
-    let addr = env::var("RABBIT_ADDR").unwrap_or_else(|_| "amqp://localhost:5672".to_string());
-    let options = ConnectionProperties::default()
-        .with_executor(tokio_executor_trait::Tokio::current())
-        .with_reactor(tokio_reactor_trait::Tokio);
-
-    let connection = Connection::connect(&addr, options).await?;
-    let channel = connection.create_channel().await?;
-
-    let _queue = channel.queue_declare(
-        "storage",
-        QueueDeclareOptions::default(),
-        FieldTable::default(),
-    );
-
-    let consumer = channel
-        .basic_consume(
-            "storage",
-            "storage",
-            BasicConsumeOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
-
-    consumer.set_delegate(move |delivery: DeliveryResult| async move {
-        let delivery = match delivery {
-            Ok(Some(delivery)) => delivery,
-            Ok(None) => return,
-            Err(error) => {
-                dbg!("Failed to consume queue message {}", error);
-                return;
-            }
-        };
-
-        delivery
-            .ack(BasicAckOptions::default())
-            .await
-            .expect("Failed to act send_webhook_event message");
-    });
-
-    Ok(())
 }
