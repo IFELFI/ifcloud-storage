6d5375a8d794151f0aa8a4788816e5b4851a9619
sauce <dadad1004@naver.com>
2024-10-10 15:02:12 +0900

    chore: change storage path tree

--- storage-manager/src/services/manage_file.rs
+++ storage-manager/src/services/manage_file.rs
@@ -10,26 +10,42 @@ pub struct ManageFileService;
 
 impl ManageFileService {
     pub async fn delete_file(file_key: &String) -> Result<()> {
+        // Constants
         let storage_path = env::var("BASE_STORAGE_PATH").unwrap_or("./storage".to_string());
+
+        // Original file path
         let file_dir_path = format!("{}/{}", storage_path, file_key);
+        // Chunked file path
+        let chunk_dir_path = format!("{}/chunks/{}", file_dir_path, file_key);
+
+        // Remove original file
         tokio::fs::remove_dir_all(file_dir_path).await?;
+        // Remove chunked file
+        tokio::fs::remove_dir_all(chunk_dir_path).await?;
 
         Ok(())
     }
 
     pub async fn merge_chunks(file_key: &String, total_chunk_count: u32) -> Result<()> {
+        // Constants
+        let original_file_name = "original";
         let storage_path = env::var("BASE_STORAGE_PATH").unwrap_or("./storage".to_string());
-        let file_dir_path = format!("{}/{}", storage_path, file_key);
-        let file_path = format!("{}{}", file_dir_path, "original");
 
+        // Chunked file path
+        let chunk_dir_path = format!("{}/chunks/{}", storage_path, file_key);
+        // Original file path
+        let original_file_path = format!("{}/{}/{}", storage_path, file_key, original_file_name);
+
+        // Read or create original file
         let mut file = tokio::fs::OpenOptions::new()
             .create(true)
             .write(true)
-            .open(file_path)
+            .open(original_file_path)
             .await?;
 
+        // Merge chunks
         for i in 0..total_chunk_count {
-            let chunk_path = format!("{}/{}", file_dir_path, i);
+            let chunk_path = format!("{}/{}", chunk_dir_path, i);
             let mut chunk = File::open(chunk_path).await?;
 
             let mut buffer = Vec::new();
@@ -37,6 +53,9 @@ impl ManageFileService {
 
             file.write_all(&buffer).await?;
         }
+
+        // Remove chunked file
+        tokio::fs::remove_dir_all(chunk_dir_path).await?;
 
         Ok(())
     }
